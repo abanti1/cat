9fee4a7bdbc8ce67531553c352ba4cb71030dc6d
abanti1 <106615480+abanti1@users.noreply.github.com>
2023-10-31 01:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,31 @@ toggleSwitch.addEventListener('change', () => {
         toggleSwitch.parentNode.style.backgroundColor = '#333';
     }
 });
+
+const showQuoteButton = document.getElementById('show-quote-button');
+const hideQuoteButton = document.getElementById('hide-quote-button');
+const quoteDisplay = document.getElementById('quote-display');
+
+const quotes = [
+    "The only way to do great work is to love what you do. -Steve Jobs",
+    "In the middle of every difficulty lies opportunity. -Albert Einstein",
+    "The best way to predict the future is to create it. -Peter Drucker",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. -Winston Churchill",
+    "The only limit to our realization of tomorrow will be our doubts of today. -Franklin D. Roosevelt"
+];
+
+showQuoteButton.addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    quoteDisplay.textContent = randomQuote;
+    quoteDisplay.style.display = 'block';
+    showQuoteButton.style.display = 'none';
+    hideQuoteButton.style.display = 'block';
+});
+
+hideQuoteButton.addEventListener('click', () => {
+    quoteDisplay.style.display = 'none';
+    hideQuoteButton.style.display = 'none';
+    showQuoteButton.style.display = 'block';
+});
+
